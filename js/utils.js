fbdb685a34c1f8b2536ef1b013d9477302a8de5a
bigfish.liu <916748598@qq.com>
2020-07-12 21:49:49 +0800

    Site updated: 2020-07-12 21:49:48

--- js/utils.js
+++ js/utils.js
@@ -1,126 +1,62 @@
-function debounce (func, wait, immediate) {
-  var timeout
-  return function () {
-    var context = this
-    var args = arguments
-    var later = function () {
-      timeout = null
-      if (!immediate) func.apply(context, args)
+/**
+ * created by lvfan
+ * 2018-09-04
+ */
+
+/**
+ * @description 获取实时时间，写入 id 为 now-time 的标签中
+ */
+(function () {
+    const divTime = document.getElementById('now-time');
+
+    function getTime() {
+        let time = new Date();
+        let hour = time.getHours() < 10 ? '0' + time.getHours() : time.getHours();
+        let minute = time.getMinutes() < 10 ? '0' + time.getMinutes() : time.getMinutes();
+        let second = time.getSeconds() < 10 ? '0' + time.getSeconds() : time.getSeconds();
+        divTime.innerText = hour + ':' + minute + ':' + second;
     }
-    var callNow = immediate && !timeout
-    clearTimeout(timeout)
-    timeout = setTimeout(later, wait)
-    if (callNow) func.apply(context, args)
-  }
-};
 
-function throttle (func, wait, options) {
-  var timeout, context, args
-  var previous = 0
-  if (!options) options = {}
+    getTime();
+    setInterval(function () {
+        getTime();
+    }, 1000);
+}());
 
-  var later = function () {
-    previous = options.leading === false ? 0 : new Date().getTime()
-    timeout = null
-    func.apply(context, args)
-    if (!timeout) context = args = null
-  }
-
-  var throttled = function () {
-    var now = new Date().getTime()
-    if (!previous && options.leading === false) previous = now
-    var remaining = wait - (now - previous)
-    context = this
-    args = arguments
-    if (remaining <= 0 || remaining > wait) {
-      if (timeout) {
-        clearTimeout(timeout)
-        timeout = null
-      }
-      previous = now
-      func.apply(context, args)
-      if (!timeout) context = args = null
-    } else if (!timeout && options.trailing !== false) {
-      timeout = setTimeout(later, remaining)
+/**
+ * @description 判断当前页面是否为活动页
+ */
+(function () {
+    // 网页当前状态判断
+    let state, visibilityChange, status, doc = document;
+    if (typeof document.hidden !== 'undefined') {
+        visibilityChange = 'visibilitychange';
+        state = 'visibilityState';
+    } else if (typeof document.mozHidden !== 'undefined') {
+        visibilityChange = 'mozvisibilitychange';
+        state = 'mozVisibilityState';
+    } else if (typeof document.msHidden !== 'undefined') {
+        visibilityChange = 'msvisibilitychange';
+        state = 'msVisibilityState';
+    } else if (typeof document.webkitHidden !== 'undefined') {
+        visibilityChange = 'webkitvisibilitychange';
+        state = 'webkitVisibilityState';
     }
-  }
-
-  return throttled
-}
-
-function sidebarPaddingR() {
-  let innerWidth = window.innerWidth
-  let clientWidth = document.body.clientWidth
-  let paddingRight = innerWidth - clientWidth
-  if (innerWidth !== clientWidth) {
-    $('body').css('padding-right', paddingRight)
-  }
-}
-
-// iPadOS
-function isIpad () {
-  return navigator.platform === 'MacIntel' && navigator.maxTouchPoints > 1 
-}
-
-function isTMobile () {
-  var ua = navigator.userAgent
-  var pa = /iPad|iPhone|iPod|Android|Opera Mini|BlackBerry|webOS|UCWEB|Blazer|PSP|IEMobile|Symbian/g
-  return window.screen.width < 992 && pa.test(ua)
-}
-
-function isMobile () {
-  return this.isIpad() || this.isTMobile()
-}
-
-function isDesktop () {
-  return !this.isMobile()
-}
-
-function scrollTo (name) {
-  var scrollOffset = $(name).offset()
-  $('body,html').animate({
-    scrollTop: scrollOffset.top
-  })
-};
-
-function loadScript (url, callback) {
-  var script = document.createElement('script')
-  script.type = 'text/javascript'
-  if (script.readyState) { // IE
-    script.onreadystatechange = function () {
-      if (script.readyState === 'loaded' ||
-        script.readyState === 'complete') {
-        script.onreadystatechange = null
-        callback()
-      }
-    }
-  } else { // Others
-    script.onload = function () {
-      callback()
-    }
-  }
-  script.src = url
-  document.body.appendChild(script)
-};
-
-function snackbarShow (text, showAction, duration) {
-  var a = (typeof showAction !== 'undefined') ? showAction : false
-  var d = (typeof duration !== 'undefined') ? duration : 2000
-  var position = GLOBAL_CONFIG.Snackbar.position
-  var bg = document.documentElement.getAttribute('data-theme') === 'light' ? GLOBAL_CONFIG.Snackbar.bgLight : GLOBAL_CONFIG.Snackbar.bgDark
-  Snackbar.show({
-    text: text,
-    backgroundColor: bg,
-    showAction: a,
-    duration: d,
-    pos: position
-  })
-}
-
-window.debounce = debounce
-
-window.throttle = throttle
-
-window.isMobile = isMobile
-
-window.loadScript = loadScript
+    let docText = doc.title;
+    // 添加监听器，在title里显示状态变化
+    doc.addEventListener(visibilityChange, function () {
+        if (doc[state] === 'visible') {
+            doc.title = '欢迎回来！d(`･∀･)b 👏';
+            status = setTimeout(() => {
+                doc.title = docText;
+            }, 1000);
+        } else {
+            doc.title = '藏起来了d(`x_x)b';
+            if (status) {
+                clearTimeout(status);
+            }
+        }
+    }, false);
+    // 初始化页面状态
+    // doc.title = '吕钒的后花园';
+}());
